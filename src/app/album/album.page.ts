7f160d15114b8994637cd95772de44ce59de7404
Tony Torian <Nemo@Tonys-MacBook-Pro-393.local>
2021-04-15 22:38:48 +0200

    w/ non func service

--- src/app/album/album.page.ts
+++ src/app/album/album.page.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { DataService } from '../core/data.service';
 import { Howl } from 'howler';
 import { IonRange } from '@ionic/angular';
-
+import { MusicPlayer } from '../core/musicPlayer.services';
 
 @Component({
   selector: 'app-album',
@@ -17,39 +17,40 @@ export class AlbumPage implements OnInit {
   authKey = '3b4187adefc05df9c4e8a848c3d296e1';
   url = 'https://settrippn.com/tunes/server/json.server.php';
   discog: any;
-  activeTrack: any[] = null;
+  // activeTrack: any[] = null;
 
 
-  player: Howl = null;
-  isPlaying = false;
-  progress = 0;
-  value = 0;
-  @ViewChild('range') range: IonRange;
+  // player: Howl = null;
+  // isPlaying = false;
+  // progress = 0;
+  // value = 0;
+  // @ViewChild('range') range: IonRange;
 
   constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private dataService: DataService) { }
 
 
-  start(track: any) { //check if playing
-    if (this.player) { //if playing stop curren track
-      this.player.stop();
-    }
-    this.player = new Howl({
-      src: [this.album?.art],
-      html5: true,
-      onplay: () => {
-        console.log('onplay');
-        this.isPlaying = true;
-        this.activeTrack = track; //keeps up with current track
-        this.updateProgress();
-      },
+  // start(track: any) { //check if playing
+  //   if (this.player) { //if playing stop curren track
+  //     this.player.stop();
+  //   }
+  //   this.player = new Howl({
+  //     src: [track.url],
+  //     html5: true,
+  //     onplay: () => {
+  //       console.log('onplay');
+  //       this.isPlaying = true;
+  //       this.activeTrack = track; //keeps up with current track
+  //       this.updateProgress();
+  //       console.log('current track', track);
+  //     },
 
-      onend: () => {
-        console.log('onend');
-      },
-    });
+  //     onend: () => {
+  //       console.log('onend');
+  //     },
+  //   });
 
-    this.player.play();
-  }
+  //   this.player.play();
+  // }
 
 
   // togglePlayer() { //when we push pause it either pauses it or plays
@@ -65,14 +66,14 @@ export class AlbumPage implements OnInit {
   //   this.isPlaying = !this.isPlaying;
   // }
 
-  updateProgress() { //slider active with time
-    let seek = this.player.seek();
-    this.progress = (seek / this.player.duration()) * 100 || 0;
-    setTimeout(() => {  //update every __ms
-      this.updateProgress();
-    }, 100);
+  // updateProgress() { //slider active with time
+  //   let seek = this.player.seek();
+  //   this.progress = (seek / this.player.duration()) * 100 || 0;
+  //   setTimeout(() => {  //update every __ms
+  //     this.updateProgress();
+  //   }, 100);
 
-  }
+  // }
 
 
 
---
+++ src/app/core/musicPlayer.services.ts
@@ -0,0 +1,68 @@
+import { Injectable, ViewChild } from '@angular/core';
+import { Howl } from 'howler';
+import { IonRange } from '@ionic/angular';
+
+
+@Injectable()
+export class MusicPlayer {
+
+    public currentTrack: any;
+
+
+    activeTrack: any[] = null;
+
+
+    player: Howl = null;
+    isPlaying = false;
+    progress = 0;
+    value = 0;
+    @ViewChild('range') range: IonRange;
+
+    public start(track: any) { //check if playing
+        if (this.player) { //if playing stop curren track
+            this.player.stop();
+        }
+        this.player = new Howl({
+            src: [track.url],
+            html5: true,
+            onplay: () => {
+                console.log('onplay');
+                this.isPlaying = true;
+                this.activeTrack = track; //keeps up with current track
+                this.updateProgress();
+                console.log('current track', track);
+            },
+
+            onend: () => {
+                console.log('onend');
+            },
+        });
+
+        this.player.play();
+    }
+
+
+    togglePlayer(pause) { //when we push pause it either pauses it or plays
+
+        if (this.isPlaying) {
+            this.player.pause();
+
+
+        }
+        else {
+            this.player.play();
+        }
+        this.isPlaying = !this.isPlaying;
+    }
+
+    updateProgress() { //slider active with time
+        let seek = this.player.seek();
+        this.progress = (seek / this.player.duration()) * 100 || 0;
+        setTimeout(() => {  //update every __ms
+            this.updateProgress();
+        }, 100);
+
+    }
+
+
+}
